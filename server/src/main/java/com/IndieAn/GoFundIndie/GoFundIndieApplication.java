fc8c2fec59d060addf5455d9625ec5cf2515ad0c
JogiYo <jo930921@gmail.com>
2021-10-22 14:32:52 +0900

    Update GoFundIndieApplication.java

--- server/src/main/java/com/IndieAn/GoFundIndie/GoFundIndieApplication.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/GoFundIndieApplication.java
@@ -2,7 +2,6 @@ package com.IndieAn.GoFundIndie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class GoFundIndieApplication {
