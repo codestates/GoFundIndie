51959ecb4b42a32402b254834922c864e6804b9a
Junhee Yi <skalook@naver.com>
2021-10-25 13:22:22 +0900

    UPDATE:Login

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -4,13 +4,38 @@ import Image from "next/image";
 import gofundIcon from "../images/gofundindie_icon.png";
 import Login from "../components/Login";
 import { useState } from "react";
+import axios from "axios";
 
 export default function Header() {
   const [loginModalOpen, setLoginModalOpen] = useState<boolean>(false);
+  const [userLoginStatus, setUserLoginStatus] = useState<boolean>(false);
   const handleLoginModal = (): void => {
-    console.log("불려짐");
     setLoginModalOpen(false);
   };
+  const handleLoginStatus = (): void => {
+    setUserLoginStatus(true);
+  };
+
+  async function Signout() {
+    try {
+      await axios(`${process.env.NEXT_PUBLIC_SERVER_URL}/signout`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Credentials": "true",
+        },
+        withCredentials: true,
+      }).then((res) => {
+        alert("로그아웃에 성공하였습니다");
+        setUserLoginStatus(false);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <>
       <header>
@@ -43,15 +68,24 @@ export default function Header() {
                 </Link>
               </li>
               <li className={styles.header__div__link}>
-                <button onClick={() => setLoginModalOpen(!loginModalOpen)}>
-                  로그인
-                </button>
+                {userLoginStatus ? (
+                  <button onClick={Signout}>로그아웃</button>
+                ) : (
+                  <button onClick={() => setLoginModalOpen(!loginModalOpen)}>
+                    로그인
+                  </button>
+                )}
               </li>
             </ul>
           </div>
         </div>
       </header>
-      {loginModalOpen ? <Login handleLoginModal={handleLoginModal} /> : null}
+      {loginModalOpen ? (
+        <Login
+          handleLoginModal={handleLoginModal}
+          handleLoginStatus={handleLoginStatus}
+        />
+      ) : null}
     </>
   );
 }
