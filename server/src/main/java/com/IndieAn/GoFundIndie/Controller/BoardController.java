4f611bd58f0887d9b2ddb94633447d21640d3de6
Chanuk Kim <flew9412@gmail.com>
2021-10-28 15:47:05 +0900

    FEAT:GraphQL
    GraphQL 연결 완료
    Query, Mutation 예시 파일 생성
    테스트 정상

--- server/src/main/java/com/IndieAn/GoFundIndie/Controller/BoardController.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Controller/BoardController.java
@@ -1,23 +1,30 @@
 package com.IndieAn.GoFundIndie.Controller;
 
+import com.IndieAn.GoFundIndie.Resolvers.User.UserGraphQLDTO;
+import com.IndieAn.GoFundIndie.Domain.Entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
 import java.util.HashMap;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
+    private final EntityManager em;
+
     private final HashMap<String, Object> body = new HashMap<>();
 
     @GetMapping(value = "/")
     public ResponseEntity<?> Test () {
+        User user = em.find(User.class, 1L);
+        String dt = UserGraphQLDTO.from(user).getNickname();
         body.clear();
-        body.put("message", "hello");
+        body.put("message", dt);
         return ResponseEntity.status(200).body(body);
     }
 }
