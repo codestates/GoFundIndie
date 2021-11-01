c1b41ccdc9db99044c5746d15414bf3a1a618b77
Chanuk Kim <flew9412@gmail.com>
2021-11-01 13:43:24 +0900

    FEAT:GraphQL
    Board 작업중
    
    WebConfig -> allowHeader 추가
    Board -> User_id 추가
    application.properties -> graphql 설정 추가

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Board/BoardResolver.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Board/BoardResolver.java
@@ -6,27 +6,29 @@ import graphql.kickstart.tools.GraphQLQueryResolver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class BoardResolver implements GraphQLQueryResolver {
     private BoardRepository boardRepository;
 
-    public String FindBoardId(Long id) {
-        System.out.println(id);
-        System.out.println(id);
-        System.out.println(id);
-        System.out.println(id);
-        System.out.println(id.getClass().getName());
-        Board board = boardRepository.findBoardId(1L);
-        if(board != null){
-            System.out.println("돼냐?");
-            System.out.println("돼냐?");
-            System.out.println("돼냐?");
-            System.out.println("돼냐?");
-            System.out.println("돼냐?");
-        }
-        return "아니 왜안돼";
-    }
+//    public String FindBoardId(Long id) {
+//        System.out.println(id);
+//        System.out.println(id);
+//        System.out.println(id);
+//        System.out.println(id);
+//        System.out.println(id.getClass().getName());
+//        Board board = boardRepository.findBoardId(1L);
+////        if(board != null){
+//            System.out.println("돼냐?");
+//            System.out.println("돼냐?");
+//            System.out.println("돼냐?");
+//            System.out.println("돼냐?");
+//            System.out.println("돼냐?");
+////        }
+//        return "아니 왜안돼";
+//    }
 }
