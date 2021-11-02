5c2e7d9b1e28e0d3f06442680c7178ebb7e3d92e
JogiYo <jo930921@gmail.com>
2021-11-02 17:45:29 +0900

    ADD: delete comment function, file:CommentController, UserController, CommentService, UserService, CommentRepository 댓글의 삭제 기능 구현 유저 서비스의 checktoken 반환형 수정, 이에 따라 UserRepository 또한 변경

--- server/src/main/java/com/IndieAn/GoFundIndie/Controller/CommentController.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Controller/CommentController.java
@@ -43,11 +43,11 @@ public class CommentController {
             }
 
             // 헤더에 존재하는 토큰을 가지고 유효성 검증을 한다.
-            Map<String, String> checkToken = userService.CheckToken(requestHeader.get("accesstoken"));
+            Map<String, Object> checkToken = userService.CheckToken(requestHeader.get("accesstoken"));
 
             // 토큰이 유효하다면 작성 기능을 수행한다.
             if(checkToken.get("email") != null) {
-                User user = userService.FindUserUseEmail(checkToken.get("email"));
+                User user = userService.FindUserUseEmail((String)checkToken.get("email"));
                 // 입력으로 들어온 userId가 DB에 존재하지 않으면 4400 응답을 한다.
                 if(userService.FindUserById(commentInputDTO.getUserId()) == null) {
                     body.put("code", 4400);
@@ -58,27 +58,11 @@ public class CommentController {
 //                    body.put("code", 4401);
 //                    return ResponseEntity.status(404).body(body);
 //                }
-
-                // 토큰으로 얻은 email의 pk와 입력으로 들어온 user의 pk가 다르다면 400 응답을 한다.
-                if(user.getId() != commentInputDTO.getUserId()) {
-                    body.put("code", 4014);
-                    return ResponseEntity.badRequest().body(body);
-                }
-
-                // comment를 작성하는 기능을 한다.
-                Comment comment = commentService.AddCommentData(commentInputDTO);
-                // 이미 comment 해당 board에 작성했다면 작성할 수 없다.
-                if(comment == null) {
-                    body.put("code", 4004);
-                    return ResponseEntity.badRequest().body(body);
-                }
-
-                body.put("code", 2000);
-                return ResponseEntity.ok().body(body);
+                body = commentService.AddCommentData(commentInputDTO, user);
+                return ResponseEntity.status(body.get("code").equals(2000) ? 200 : 400).body(body);
             }
             else {
-                body.put("code", checkToken.get("code"));
-                return ResponseEntity.status(401).body(body);
+                return ResponseEntity.status(401).body(checkToken);
             }
         } catch (Exception e) {
             return ResponseEntity.status(500).body("err");
@@ -86,13 +70,40 @@ public class CommentController {
     }
 
     @GetMapping(value = "/comment/{boardId}")
-    public ResponseEntity<?> GetComments(@PathVariable Long boardId, @PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 10) Pageable pageable) {
+    public ResponseEntity<?> GetComments(@PathVariable long boardId, @PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 10) Pageable pageable) {
         // 영화 보드에 작성된 댓글들을 불러오는 메소드
         // 해당 board가 존재하지 않으면 404 응답을 한다.
         try {
             body.clear();
             body = commentService.GetCommentPage(boardId, pageable);
             return ResponseEntity.status(body.get("data") == null ? 404 : 200).body(body);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("err");
+        }
+    }
+
+    @DeleteMapping(value = "/comment")
+    public ResponseEntity<?> DeleteComment(@RequestParam("comment_id") long commentId, @RequestHeader Map<String, String> requestHeader)  {
+        // 작성된 댓글을 삭제하는 기능
+        // 해당 id를 가진 comment가 존재하지 않으면 404 응답을 한다.
+        try {
+            // 헤더에 accesstoken이 없으면 4000 응답을 한다.
+            if(requestHeader.get("accesstoken") == null) {
+                body.put("code", 4000);
+                return ResponseEntity.badRequest().body(body);
+            }
+
+            // 헤더에 존재하는 토큰을 가지고 유효성 검증을 한다.
+            Map<String, Object> checkToken = userService.CheckToken(requestHeader.get("accesstoken"));
+            User user = userService.FindUserUseEmail((String) checkToken.get("email"));
+            // token에 email정보가 있다면 댓글 삭제 과정을 수행한다
+            if(checkToken.get("email") != null) {
+                body = commentService.DeleteComments(commentId, user);
+                return ResponseEntity.status(body.get("code").equals(2000) ? 200 : 404).body(body);
+            }
+            else {
+                return ResponseEntity.status(401).body(checkToken);
+            }
         } catch (Exception e) {
             return ResponseEntity.status(500).body("err");
         }
--- server/src/main/java/com/IndieAn/GoFundIndie/Repository/CommentRepository.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Repository/CommentRepository.java
@@ -32,6 +32,17 @@ public class CommentRepository {
         return entityManager.createQuery("SELECT c FROM Comment as c where c.board_id = '"+ boardId +"'", Comment.class).getResultList();
     }
 
+    // (임시) boardId를 통해 board를 찾는 기능
+    public Board FindBoardDB(long boardId) {
+        return entityManager.find(Board.class, boardId);
+    }
+
+    // Id를 통해서 Comment를 찾는 기능
+    public Comment FindCommentById(long commentId) {
+        return entityManager.find(Comment.class, commentId);
+//        return entityManager.createQuery("SELECT c FROM Comment as c where c.id = '"+ commentId +"'", Comment.class).getResultList().get(0);
+    }
+
     // DB Comment 테이블에 매개변수 commentInputDTO의 데이터를 사용하여 Comment 정보를 저장한다.
     public Comment AddComment(CommentInputDTO commentInputDTO) {
         Comment comment = new Comment();
@@ -54,7 +65,14 @@ public class CommentRepository {
         return comment;
     }
 
-    public Board FindBoardDB(long boardId) {
-        return entityManager.createQuery("SELECT b FROM Board as b where b.id = '"+ boardId +"'", Board.class).getResultList().get(0);
+    // DB Comment 테이블에 매개변수 commentId를 사용하여 Comment 정보를 삭제한다.
+    public Comment DeleteComment(long commentId) {
+        Comment deleteComment = entityManager.find(Comment.class, commentId);
+        entityManager.remove(deleteComment);
+
+        entityManager.flush();
+        entityManager.close();
+
+        return deleteComment;
     }
 }
