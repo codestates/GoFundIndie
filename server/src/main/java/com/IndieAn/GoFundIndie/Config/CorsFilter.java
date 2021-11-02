aba32b12ec993b3f1c4bf652cdf396d73c0ccbbc
Chanuk Kim <flew9412@gmail.com>
2021-11-02 14:47:49 +0900

    EDIT:CORS
    CorsFilter -> Header 에서 Origin 받는거로 변경
    Board.java -> nullable 삭제
    BoardController -> HealthController health check 옮김

--- server/src/main/java/com/IndieAn/GoFundIndie/Config/CorsFilter.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Config/CorsFilter.java
@@ -1,5 +1,6 @@
 package com.IndieAn.GoFundIndie.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -12,23 +13,33 @@ import java.io.IOException;
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+    @Value("#{info['gofund.url1']}")
+    private String url1;
+
+    @Value("#{info['gofund.url2']}")
+    private String url2;
+
+    @Value("#{info['gofund.url3']}")
+    private String url3;
+
+    @Value("#{info['gofund.url4']}")
+    private String url4;
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
+    public void init(FilterConfig filterConfig) throws ServletException {}
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Methods, Access-Control-Allow-Origin,Access-Control-Allow-Headers, Access-Control-Allow-Credentials");
+        String ori = request.getHeader("Origin");
+        if(ori.equals(url1) || ori.equals(url2) || ori.equals(url3) || ori.equals(url4)){
+            response.setHeader("Access-Control-Allow-Origin", ori);
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods","*");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "*");
+        }
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
@@ -38,7 +49,5 @@ public class CorsFilter implements Filter {
     }
 
     @Override
-    public void destroy() {
-
-    }
+    public void destroy() {}
 }
