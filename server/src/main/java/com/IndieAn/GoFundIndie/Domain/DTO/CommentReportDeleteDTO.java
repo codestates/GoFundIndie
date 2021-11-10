2e7c565c836163b20c943ae0f1dfe448d9131c0b
JogiYo <jo930921@gmail.com>
2021-11-10 11:36:53 +0900

    ADD: comment report function, file: CommentController, CommentService, CommentReportRepository, All CommentReportDTO 코멘트 신고하기 기능 추가 관리자 일 때 신고내역을 삭제하는 기능 또한 추가

---
+++ server/src/main/java/com/IndieAn/GoFundIndie/Domain/DTO/CommentReportDeleteDTO.java
@@ -0,0 +1,9 @@
+package com.IndieAn.GoFundIndie.Domain.DTO;
+
+public class CommentReportDeleteDTO {
+    private long reportId;
+
+    public long getReportId() {
+        return reportId;
+    }
+}
