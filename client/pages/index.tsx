9fec78fed7512c1fb07956b9fabcda9cd3ebef99
Junhee Yi <skalook@naver.com>
2021-11-11 14:19:11 +0900

    internererror

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,7 +6,7 @@ import { GetServerSideProps } from "next";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export default function Page({ film }: any) {
-  if (film === null) {
+  if (film === null || film.FindRandomBoard === null) {
     return <div className={styles.error}> 서버에 오류가 발생했습니다.</div>;
   }
   const recommend: any = film.FindRandomBoard.data;
@@ -29,6 +29,8 @@ export default function Page({ film }: any) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookies: any = context.req.headers.cookie;
+
   const query = `{
 FindRandomBoard (Limit: 20) {
   code
@@ -54,6 +56,7 @@ FindRandomBoard (Limit: 20) {
     body: JSON.stringify({ query }),
     headers: {
       "Content-Type": "application/json",
+      accesstoken: cookies?.slice(cookies?.search("accesstoken") + 12),
     },
   }).catch((err) => {
     return err;
@@ -61,6 +64,7 @@ FindRandomBoard (Limit: 20) {
   if (res.code === "ECONNREFUSED") return { props: { film: null } };
   const film = await (await res).json();
   if (!film) return { props: {} };
+  console.log(film);
   return {
     props: {
       film: film.data,
