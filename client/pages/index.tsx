fdf2499e016ad22155306a019bc120c3e38bd872
Junhee Yi <skalook@naver.com>
2021-11-11 15:55:30 +0900

    token

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -50,7 +50,8 @@ FindRandomBoard (Limit: 20) {
   }
 }
 }`;
-
+  console.log(cookies);
+  console.log(cookies?.slice(cookies?.search("accesstoken") + 12));
   const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/graphql`, {
     method: "POST",
     body: JSON.stringify({ query }),
